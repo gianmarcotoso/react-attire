637285576eea70e12a0ecc409ec7a2b86a0cc25a
Gian Marco Toso <info@gianmarcotoso.com>
2018-01-11 17:16:45 +0100

    add build files

--- dist/validate.d.ts
+++ dist/validate.d.ts
@@ -1,4 +1,4 @@
 import { ValidationRules } from './contracts';
-declare function validate<S = object>(rules: ValidationRules<S>): (data: S) => object;
-declare function validate<S = object>(rules: ValidationRules<S>, data: S): object;
+declare function validate<S = object>(rules: ValidationRules<S>): (data: S) => Promise<object>;
+declare function validate<S = object>(rules: ValidationRules<S>, data: S): Promise<object>;
 export { validate };
