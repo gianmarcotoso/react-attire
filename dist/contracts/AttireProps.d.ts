6182a22301835a1624d3790feb309b3ade67719b
Gian Marco Toso <info@gianmarcotoso.com>
2018-05-25 15:34:39 +0200

    build production version

--- dist/contracts/AttireProps.d.ts
+++ dist/contracts/AttireProps.d.ts
@@ -2,7 +2,7 @@ import { RenderFunction } from '../types/RenderFunction';
 interface AttireProps<FormState> {
     initial?: Partial<FormState>;
     onChange?(data: FormState): void;
-    onInitialChange?(data: FormState, prevInitial: Partial<FormState>): FormState;
+    onInitialChange?(data: FormState, prevInitial: Partial<FormState>, currentInitial: Partial<FormState>): FormState | false;
     children: RenderFunction<FormState>;
 }
 export { AttireProps };
